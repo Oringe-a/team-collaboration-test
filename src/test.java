5f355cea1247922afda9ee4194c82c2a5214c8c9
kakamo-129 <1024nina17@gmail.com>
2025-12-06 23:50:24 +0800

    -

---
+++ src/test.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args){
+    System.out.println("Welcome to Douban Movie Network!");
+        System.out.println("----------------------------------");
+        System.out.println("The system is being initialized...");
+        System.out.println("----------------------------------");
+        System.out.println("Please log in first!");
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your username:");
+    String username = scanner.nextLine();
+        System.out.println("Please enter your password:");
+    int password = scanner.nextInt();
+    String name = "Qijie,Wang";
+    int key = 123456;
+        if (username == name && password == key){
+        System.out.println("Login successfully!");
+    }
+        else {
+        System.out.println("Wrong,try please again");
+    }
+
+}
+}
